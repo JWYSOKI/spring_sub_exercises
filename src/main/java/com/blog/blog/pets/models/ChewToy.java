cd8cd238a00aaebd531cb39737079c599cf44760
Jillian Wysoki <jillianwysoki@gmail.com>
2018-09-12 10:51:50 -0500

    first commit for spring sub-exercises'

--- src/main/java/com/blog/blog/pets/models/ChewToy.java
+++ src/main/java/com/blog/blog/pets/models/ChewToy.java
@@ -22,4 +22,48 @@ public class ChewToy implements Serializable {
 
     // add relationships, constructors, getters and setters...
 
+
+    public ChewToy(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
